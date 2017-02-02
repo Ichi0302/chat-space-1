7fc47f2e24cab1e9bbae2c93bf5915aa8aac48dd
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-02 18:04:06 +0900

    make_groupuser_model

---
+++ db/migrate/20170202090047_create_groupusers.rb
@@ -0,0 +1,9 @@
+class CreateGroupusers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :groupusers do |t|
+      t.references :user, foreign_key: true
+      t.references :group, foreign_key: true
+      t.timestamps
+    end
+  end
+end
