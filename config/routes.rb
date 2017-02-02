9b1665afc2a2273788f7c278104ec845f7c2390e
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-02 20:20:11 +0900

    edit_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
   resources :messages, only: [:index]
   root 'messages#index'
 end
