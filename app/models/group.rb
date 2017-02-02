dc51abdfc5a5355dd7f64ea5317911b5069cbe50
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-02 18:08:35 +0900

    add_association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  has_many :groupusers
+  has_many :users, through: :groupusers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
+
+  has_many :groupusers
+  has_many :groups, through: :groupusers
 end
