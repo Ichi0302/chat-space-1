7288b39948400c1b45076b0e12c84b817d5dccca
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-05 19:44:19 +0900

    edit_messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,23 @@ class MessagesController < ApplicationController
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
     @messages = @group.messages
+    @message = Message.new
   end
+
+
+  def create
+    @message = Message.new(create_params)
+    if @message.save
+      redirect_to root_path
+    else
+      flash.alert = 'メッセージを入力してください。'
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:message).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
 end
