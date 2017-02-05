f81d9b1a2922b8610c68720ea6bad158c687d3cb
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-05 19:16:02 +0900

    edit_views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,6 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
+    @messages = @group.messages
   end
 end
