b943b8f0bdba38217c3cbe6c55f726e41114a558
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-09 17:01:32 +0900

    add faker

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
   factory :message do
-    text         "test message"
-    image        "test image"
-    user_id       1
-    group_id      1
+    text       { Faker::StarWars.quote }
+    image      { Faker::File.file_name('path/to') }
+    user_id    { Faker::Number.number(1) }
+    group_id   { Faker::Number.number(1) }
     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 
