2c0834682703f1a36e9b79a56c6b830300df4403
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-13 21:57:42 +0900

    fix messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group) }
-        format.json {}
+        format.json
       end
     else
       flash.alert = 'メッセージを入力してください。'
