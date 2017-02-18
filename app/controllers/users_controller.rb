abd2749652f1f5bd6d8e6a953cd79554f83a8624
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-18 09:45:17 +0900

    edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def search
-    @users = User.where('name LIKE(?)', "#{params[:user][:name]}%")
+    @users = User.where('name LIKE(?)', "#{params[:name]}%").where.not(name: current_user.name)
     respond_to do |format|
       format.json
     end
