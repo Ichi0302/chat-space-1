f60398b6c2c803af3717e9a4499371192b764297
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-02-19 13:35:04 +0900

    add function to group.js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -2,20 +2,29 @@ $(document).on('turbolinks:load', function() {
   $('#user-search-field').on("keyup", function() {
     AjaxSearch();
   });
+  $(document).on("click", '.adding-group-user__btn', function() {
+    var user = $(this).parent();
+    AddingUser(user);
+  });
+  $(document).on("click", '.chat-group-user__btn', function() {
+    var user = $(this).parent();
+    RemoveUser(user);
+  });
 });
 
 function buildAddingList(user) {
-  var list = `<div class="adding-group-user">
-                  <p class="adding-group-user__name">${ user.name }</p>
-                  <a class="adding-group-user__btn">追加</a>
+  var list = `<div class="adding-group-user" data-id="${user.id}" data-name="${user.name}">
+                <p class="adding-group-user__name">${ user.name }</p>
+                <a class="adding-group-user__btn">追加</a>
               </div>`;
   return list;
 }
 
-function buildAddedList(user) {
-  var list = `<li class="chat-group-user">
-                  <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="chat-group-user__btn">削除</a>
+function buildAddedList(id, name) {
+  console.log("hjhhhhh")
+  var list = `<li class="chat-group-user" data-id="${id}"><input type="hidden" name="group[user_ids][]" value="${id}" />
+                <p class="chat-group-user__name">${name}</p>
+                <a class="chat-group-user__btn">削除</a>
               </li>`;
   return list;
 }
@@ -41,4 +50,16 @@ function AjaxSearch() {
     alert('エラーが発生しました。');
   });
   return false;
+}
+
+function AddingUser(user) {
+  var id   = user.data("id");
+  var name = user.data("name");
+  user.remove();
+  var list = buildAddedList(id, name);
+  $("#group-users").append(list);
+}
+
+function RemoveUser(user){
+  user.remove();
 }
