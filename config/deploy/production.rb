469d3d07f84253cfaf942efbf1f168834aa2efb4
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-03-17 12:19:07 +0900

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.64.160.190', user: 'chat-space-app', roles: %w{app db web}
+server '54.64.160.190', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
