081794bc794fe24de457df8c88d4626f6ba09fe6
Hiroaki Sumizaki <hiroakisumizaki@Hiroakis-MacBook-Pro.local>
2017-03-24 21:35:56 +0900

    delete migration files

--- db/migrate/20170119130756_remove_column_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnFromUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :name, :string
-    remove_column :users, :email, :string
-  end
-end
--- db/migrate/20170119131158_add_column_to_users.rb
+++ db/migrate/20170119131158_add_column_to_users.rb
@@ -1,6 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :email, :string, null: false, default: ""
     add_column :users, :name, :string, null: false, index: true, default: ""
   end
 end
